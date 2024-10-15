61580031e892c7a4d8e027ab910de400c3c43bf3
JerryKiki <gasi758@gmail.com>
2024-10-15 16:52:27 +0900

    에러고침, db.sql 설정

--- DB.sql
+++ DB.sql
@@ -2,10 +2,14 @@ DROP DATABASE IF EXISTS SpringBatch_10;
 CREATE DATABASE SpringBatch_10;
 USE SpringBatch_10;
 
-DROP DATABASE IF EXISTS SpringBatch_backup;
+DROP DATABASE IF EXISTS SpringBatch_10_backup;
 CREATE DATABASE SpringBatch_10_backup;
 USE SpringBatch_10_backup;
 
 SHOW TABLES;
 
 ### Spring 실행 시 추가되는 테이블 : Batch 기본 테이블(기록용), 커스텀 테이블과 섞어쓴다.
+
+# mysqldump -u root SpringBatch_10 | mysql -u root SpringBatch_10_backup << xampp에서 shell 열어서 백업하는 법
+# mysqldump -u root SpringBatch_10 > SpringBatch_10.sql << xampp에서 shell 열어서 sql 파일로 만드는 법 (데이터 및 쿼리 저장)
+# mysql -u root SpringBatch_10_backup < SpringBatch_10.sql << xampp에서 shell 열어서 백업에 sql 파일을 집어넣는 법
--- src/main/java/com/lyj/proj/springbatch_10/job/HelloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/lyj/proj/springbatch_10/job/HelloWorld/HelloWorldJobConfig.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -12,30 +13,34 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class HelloWorldConfig {
+public class HelloWorldJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    //위에꺼랑 아래꺼랑 매칭시키기!
-    //아래 코드가 기본꼴이다. ==> 하나의 단위로서, 전부 순차적으로 실행이 되어야함. 트랜젝션과 비슷
-    //Job : 여러가지의 Step들로 구성
     @Bean
     public Job helloWorldJob() {
-        return jobBuilderFactory.get("HelloWorldJob").start(helloWorldStep1()).build();
+        return jobBuilderFactory.get("helloWorldJob")
+                .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행할 때 파라미터로 부여
+                .start(helloWorldStep1()).build();
     }
 
     @Bean
     public Step helloWorldStep1() {
-        return stepBuilderFactory.get("HelloWorldStep1").tasklet(helloWorldTasklet()).build();
+        return stepBuilderFactory.get("helloWorldStep1").tasklet(helloWorldTasklet()).build();
     }
 
     @Bean
     public Tasklet helloWorldTasklet() {
         return (stepContribution, chunkContext) -> {
-            System.out.println("HelloWorld!!!");
+            System.out.println("헬로월드!!!");
             return RepeatStatus.FINISHED;
         };
     }
 
+    //Job :여러가지의  Step들로 구성
 
 }
+
+//위에꺼랑 아래꺼랑 매칭시키기!
+//아래 코드가 기본꼴이다. ==> 하나의 단위로서, 전부 순차적으로 실행이 되어야함. 트랜젝션과 비슷
+//Job : 여러가지의 Step들로 구성
